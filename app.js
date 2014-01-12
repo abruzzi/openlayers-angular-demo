02166049e6e3cd35ebb6063ab6abff7fcb874671
Qiu Juntao <juntao.qiu@gmail.com>
2014-01-12 17:30:59 +1100

    first version that does not work, and the baseline I'll be working on

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module('OpenLayersApp', []);
+
+app.controller('EventController', 
+        ['$scope', '$compile', function($scope, $compile) {
+
+    var map = new OpenLayers.Map("map",{projection:"EPSG:3857"});
+    var osm = new OpenLayers.Layer.OSM();
+    var toMercator = OpenLayers.Projection.transforms['EPSG:4326']['EPSG:3857'];
+    var center = toMercator({x:-0.05,y:51.5});
+
+    map.addLayers([osm]);
+    map.setCenter(new OpenLayers.LonLat(center.x,center.y), 13);
+
+    $scope.time = new Date();
+    $scope.text = "Sun Mansi";
+
+    $scope.showPopup = function() {
+        var html = "<div><span>{{time}}</span><span>{{text}}</span></div>";
+        var content = $compile(html)($scope);
+
+        var popup = new OpenLayers.Popup.FramedCloud("popup",
+            OpenLayers.LonLat.fromString("-5694.06868525478, 6708925.0877411375"),
+            null,
+            content.html(),
+            null,
+            true
+        );  
+        map.addPopup(popup);    
+    };
+}]);
